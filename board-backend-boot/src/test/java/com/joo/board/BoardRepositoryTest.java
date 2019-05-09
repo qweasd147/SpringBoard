c197d8e35e491f8bf934a4e4fcd7bf77933e6d5d
joohyung <qweasd_147@naver.com>
2019-05-09 14:44:58 +0900

    Repository test 추가

--- board-backend-boot/src/test/java/com/joo/board/BoardRepositoryTest.java
+++ board-backend-boot/src/test/java/com/joo/board/BoardRepositoryTest.java
@@ -1,43 +1,66 @@
 package com.joo.board;
 
 import com.joo.common.state.CommonState;
+import com.joo.model.dto.BoardWriteRequestDto;
 import com.joo.model.entity.BoardEntity;
 import com.joo.model.entity.FileEntity;
 import com.joo.repository.BoardRepository;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
-@DataJpaTest
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class BoardRepositoryTest {
 
     @Autowired
     private BoardRepository boardRepository;
 
+    private BoardEntity saveNormalBoard;
 
-    //TODO : 현재 테스트 코드 샘플 형식으로 작성. 추후 정확한 값이 나오도록 셋팅
+    @After
+    public void deleteAll(){
+        boardRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("정상적인 게시물 입력 테스트")
+    public void a_registBoard(){
+        BoardWriteRequestDto normalBoardWriteRequestDto = BoardWriteRequestDto.builder()
+                .subject("Board Repository test subject")
+                .contents("Board Repository test Contents")
+                .build();
+
+        saveNormalBoard = boardRepository.save(normalBoardWriteRequestDto.toEntity());
+    }
+    
 
     @Test
     @DisplayName("정상적으로 사용가능한 게시물 검색 테스트")
-    public void findEnableBoardTest(){
-        long boardIndex = 15;
+    public void b_findEnableBoardTest(){
 
-        BoardEntity boardEntity = boardRepository.findEnableBoardByBoardIdx(boardIndex)
+        a_registBoard();
+
+        BoardEntity searchBoardEntity = boardRepository.findEnableBoardByBoardIdx(saveNormalBoard.getIdx())
                 .orElse(BoardEntity.builder().build());
 
-        assertThat(boardEntity.getIdx()).isEqualTo(boardIndex);
-        assertThat(boardEntity.getState()).isEqualTo(CommonState.ENABLE);
+        assertThat(searchBoardEntity.getIdx()).isEqualTo(saveNormalBoard.getIdx());
+        assertThat(searchBoardEntity.getState()).isEqualTo(CommonState.ENABLE);
 
-        List<FileEntity> fileList = boardEntity.getFileList();
+        assertThat(searchBoardEntity.getSubject()).isEqualTo(saveNormalBoard.getSubject());
+        assertThat(searchBoardEntity.getContents()).isEqualTo(saveNormalBoard.getContents());
+
+
+        List<FileEntity> fileList = saveNormalBoard.getFileList();
 
         //검색된 파일이 모두 사용 가능한것만 검색 된건지 테스트
         boolean isDisableFileExist = fileList.stream()
@@ -45,6 +68,6 @@ public class BoardRepositoryTest {
                 .filter(fileState -> fileState != CommonState.ENABLE)
                 .findAny().isPresent();
 
-        assertTrue(isDisableFileExist);
+        assertFalse(isDisableFileExist);
     }
 }
