8acca80b49968a6e88384eb6d3e0cf38788e9fe7
joohyung <qweasd_147@naver.com>
2019-05-31 18:40:39 +0900

    프로퍼티 파일 yml로 마이그레이션, 로그인 oauth 서버 정보 프로퍼티 파일 분리

--- board-backend-boot/src/main/java/com/joo/Application.java
+++ board-backend-boot/src/main/java/com/joo/Application.java
@@ -1,6 +1,5 @@
 package com.joo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -8,23 +7,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class Application {
 
-	/*
 	static final String PROPERTIES = "spring.config.location="
-		+ "classpath:thirdparty.request.properties,"
-		+ "classpath:thirdparty.security.properties";
-	*/
-
-
-	//private static final String PROPERTIES = "spring.config.location=classpath:login.security.yml";
-	private static final String PROPERTIES = "spring.config.location=classpath:/login.security.yml";
-
+		+ "classpath:application.yml,"
+		+ "classpath:login.security.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		/*
+		//SpringApplication.run(Application.class, args);
 		new SpringApplicationBuilder(Application.class)
 				.properties(PROPERTIES)
 				.run(args);
-				*/
 	}
 }
